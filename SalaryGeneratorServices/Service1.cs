edbc2c1edf805cb408c89172bd827e9ace5ed1ac
ashahri90 <ashahri90@gmail.com>
2024-06-03 07:40:00 +0800

    Array list exclude activity TL

--- SalaryGeneratorServices/Service1.cs
+++ SalaryGeneratorServices/Service1.cs
@@ -154,7 +154,7 @@ namespace SalaryGeneratorServices
                     incentifsType = Step1Func.GetIncentifsTypeFunc(NegaraID, SyarikatID);
                     var bonusHarian = Step1Func.GetBonusHarianBukanMenuai(NegaraID, SyarikatID);
                     var tbl_HargaSawitSemasa = Step1Func.GetHargaSawitSemasa(NegaraID, SyarikatID, Month, Year);
-                    string[] flag1 = new string[] { "kadarot", "kiraot", "cuti", "sbbTakAktif" };
+                    string[] flag1 = new string[] { "kadarot", "kiraot", "cuti", "sbbTakAktif", "aktvtexcludeTL" };
                     List<tblOptionConfigsWeb> tblOptionConfigsWeb = Step1Func.GetWebConfigList(flag1, NegaraID, SyarikatID);
                     var tbl_GajiMinimaLdg = Step1Func.GetGajiMinimaLdg(LadangID);
                     var tbl_UpahAktiviti = Step1Func.tbl_UpahAktiviti(NegaraID, SyarikatID);
@@ -465,7 +465,7 @@ namespace SalaryGeneratorServices
                             Step1Func.UpdateServicesProcessPercFunc(SevicesProcess, Percentage, 1, db, TotalDataCount2, DataCount2);
                             WriteLog2(Log, ServiceName, ServiceProcessID);
 
-                            Step6Func.GetDebitCreditBalanceFunc(NegaraID, SyarikatID, WilayahID, LadangID, UserID, DateTimeFunc.GetDateTime(), Month, Year, getservicesdetail.fld_SevicesActivity, getservicesdetail.fld_ServicesName, getservicesdetail.fld_ClientID, out Log);
+                            Step6Func.GetDebitCreditBalanceFunc(NegaraID, SyarikatID, WilayahID, LadangID, UserID, DateTimeFunc.GetDateTime(), Month, Year, getservicesdetail.fld_SevicesActivity, getservicesdetail.fld_ServicesName, getservicesdetail.fld_ClientID, tblOptionConfigsWeb, out Log);
                             DataCount = DataCount + 1;
                             DataCount2 = DataCount2 + 1;
                             //Percentage = Math.Round((LoopCountData++ / CountData) * 100, 2); //commented by faeza 08.10.2021
